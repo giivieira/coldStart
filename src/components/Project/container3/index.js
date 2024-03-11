46a468e6de93fa5d9bf28fb1ddeadc9ec77f0ca4
Your Name <you@example.com>
2024-03-11 16:47:52 -0300

    11/03/2024

--- src/components/Project/container3/index.js
+++ src/components/Project/container3/index.js
@@ -6,11 +6,20 @@ import figma from '../../../assets/figma.png'
 
 function Container3 () {
   return (
-    <div className='container3'>
+    <div className='container3' 
+    style={{ width: '100vw', height: '500px', backgroundColor: '#0067C7' }}>
       <div className='title'>
         <h1>TECHNOLOGIES USED</h1>
       </div>
+      <div className='imagensTecnologias'>
+        <img src={react} alt="react"></img>
+        <img src={vite} alt="vite"></img>
+        <img src={figma} alt="figma"></img>
+      </div>
     </div>
+
+      
+    
   )
 }
 

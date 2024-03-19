6ba63137501c97d9f8f486a1647a6a6ef73b7ed3
Your Name <you@example.com>
2024-03-19 12:32:25 -0300

    19/03/2024

--- src/components/Project/team/index.js
+++ src/components/Project/team/index.js
@@ -5,46 +5,47 @@ import giovanna from "../../../assets/giovanna.png";
 import diego from "../../../assets/diego.png";
 import linkedin from "../../../assets/linkedin.png";
 
+
+const teamMembers = [
+  {
+    name: 'Ângelo Carnevale',
+    function: 'Fullstack Developer',
+    photo: angelo,
+    linkedin: 'https://www.linkedin.com/in/%C3%A2ngelo-carnevale-649ba2229/',
+  },
+  {
+    name: 'Diego Castan',
+    function: 'BackEnd Developer',
+    photo: diego,
+    linkedin: 'https://www.linkedin.com/in/diego-castan-lopes/',
+  },
+  {
+    name: 'Giovanna Vieira ',
+    function: 'UX/UI Designer',
+    photo: giovanna,
+    linkedin: 'https://www.linkedin.com/in/giovanna-vieira-b35676266/',
+  },
+];
+
 function TheTeam() {
   return (
     <div className="containerTeam">
       <h1>THE TEAM</h1>
       <div className="fotos">
-        <img id="angelo" src={angelo} alt="angelo"></img>
-        <a
-          href="https://www.linkedin.com/in/%C3%A2ngelo-carnevale-649ba2229/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img id="linkedinAngelo" src={linkedin} alt="linkedin"></img>
-        </a>
-
-        <img id="diego" src={diego} alt="diego"></img>
-        <a
-          href="https://www.linkedin.com/in/diego-castan-lopes/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img id="linkedinDiego" src={linkedin} alt="linkedin"></img>
-        </a>
-
-        <img id="giovanna" src={giovanna} alt="giovanna"></img>
-        <a
-          href="https://www.linkedin.com/in/giovanna-vieira-b35676266/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img id="linkedinGiovanna" src={linkedin} alt="linkedin"></img>
-        </a>
-      </div>
-
-      <div className="nomes">
-        <p id="name">Ângelo Carnevale</p>
-        {/* <p id='function'>Fullstack Developer</p> */}
-        <p id="name">Diego Castan</p>
-        {/* <p id='function'>BackEnd Developer</p> */}
-        <p id="name">Giovanna Vieira</p>
-        {/* <p id='function'>UX/UI Designer</p> */}
+        {teamMembers.map((member, index) => (
+          <div key={index} className="teamMember">
+            <div className="imageContainer">
+              <img src={member.photo} alt={member.name}></img>
+              <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
+                <img className="linkedinIcon" src={linkedin} alt="linkedin"></img>
+              </a>
+            </div>
+            <div className={`info${member.name.split(' ')[0]}`}>
+              <p>{member.name}</p>
+              <p>{member.function}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
